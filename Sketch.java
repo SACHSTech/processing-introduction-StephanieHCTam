8439bc152fc754538ae1e9b8a98b4bedbf96fcbb
Stephanie Tam <78435360+StephanieHCTam@users.noreply.github.com>
2022-02-22 14:31:55 +0000

    add mouse drawn circles

--- Sketch.java
+++ Sketch.java
@@ -30,6 +30,13 @@ public class Sketch extends PApplet {
 
     stroke(255);
     line(50, 125, 70, 50);  
+
+    if (mousePressed) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
   }
   
   // define other methods down here.
